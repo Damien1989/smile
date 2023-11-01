b5e2ae23ee026ca977744fbf170a9e15b157016e
damien <contact.damiendiaz@gmail.com>
2023-11-01 18:08:42 +0100

    Navbar and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Home from './pages/Home';
+import Main from './pages/Main';
 import './css/app.css';
+import Navbar from './pages/Navbar';
 
 function App() {
 return (
+  <div>
+    <Navbar />
   <Home />
+  <Main />
+  </div>
 )
 }
 
---
+++ src/pages/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+    return (
+        <div className="main">
+        <h1>Welcome to the South</h1>
+        <button>I'm a button</button>
+        </div>
+    )
+}
