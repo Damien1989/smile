bfb2d635b1043b60f73eea0ded1bfb7cbb15a053
damien <contact.damiendiaz@gmail.com>
2023-11-01 19:49:40 +0100

    modif navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -23,45 +23,18 @@ export default function Navbar() {
     <MDBNavbar expand='lg' light bgColor='light'>
       <MDBContainer fluid>
         <MDBNavbarBrand href='#'>GROUPE</MDBNavbarBrand>
+        <MDBNavbarBrand href='#'>OFFRES</MDBNavbarBrand> 
+        <MDBNavbarBrand href='#'>RÉFÉRENCES</MDBNavbarBrand>
+        <MDBNavbarBrand href='#'>TECHNOLOGIES</MDBNavbarBrand>
+        <MDBNavbarBrand href='#'>LIVRES BLANCS</MDBNavbarBrand>
+        <MDBNavbarBrand href='#'>INNOVATIONS</MDBNavbarBrand>
+        <MDBNavbarBrand href='#'></MDBNavbarBrand>
+        
 
-        <MDBNavbarToggler
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowBasic(!showBasic)}
-        >
-          <MDBIcon icon='bars' fas />
-        </MDBNavbarToggler>
+     
 
         <MDBCollapse navbar show={showBasic}>
           <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='#'>
-                OFFRES
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Link</MDBNavbarLink>
-            </MDBNavbarItem>
-
-            <MDBNavbarItem>
-              <MDBDropdown>
-                <MDBDropdownToggle tag='a' className='nav-link' role='button'>
-                  Dropdown
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <MDBDropdownItem link>Action</MDBDropdownItem>
-                  <MDBDropdownItem link>Another action</MDBDropdownItem>
-                  <MDBDropdownItem link>Something else here</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavbarItem>
-
-            <MDBNavbarItem>
-              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                Disabled
-              </MDBNavbarLink>
-            </MDBNavbarItem>
           </MDBNavbarNav>
 
           <form className='d-flex input-group w-auto'>
