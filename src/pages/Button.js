271ac860f635017c96e16770149877d5a2d3b10e
damien <contact.damiendiaz@gmail.com>
2023-11-09 21:56:14 +0100

    add text

--- src/pages/Button.js
+++ src/pages/Button.js
@@ -1,7 +1,7 @@
 export default function Button() {
     return (
         <div className="button">
-            <button variant="success">Découvrir nos projets</button>
+            <button variant="success">Découvrir nos projets...</button>
         </div>
     )
 }
